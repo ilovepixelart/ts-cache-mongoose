bc291de073fca47cd98b3d69d98bb91f95ef6c7f
Alex Eagle <theherocraft@gmail.com>
2023-04-28 00:50:55 +0200

    Refactor hashing

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,27 +1,10 @@
-import _ from 'lodash'
 import { createHash } from 'crypto'
-
-export function sortDeep (data: Record<string, unknown>[] | Record<string, unknown>): unknown {
-  if (_.isObjectLike(data) && _.isArray(data)) {
-    return data.map(sortDeep)
-  }
-
-  if (_.isObjectLike(data) && !_.isArray(data)) {
-    const sortedKeys = _.keys(data).sort((a, b) => a.localeCompare(b))
-    const sortedObj: Record<string, unknown> = {}
-
-    sortedKeys.forEach((key) => {
-      sortedObj[key] = sortDeep(data[key] as Record<string, unknown>[] | Record<string, unknown>)
-    })
-
-    return sortedObj
-  }
-
-  return data
-}
+import sortKeys from 'sort-keys'
 
 export function getKey (data: Record<string, unknown>[] | Record<string, unknown>): string {
-  const sortedObj = sortDeep(data)
-  const sortedStr = JSON.stringify(sortedObj)
-  return createHash('sha256').update(sortedStr).digest('hex')
+  const sortedObj = sortKeys(data, { deep: true })
+  const sortedStr = JSON.stringify(sortedObj, (_key, val: unknown) => {
+    return val instanceof RegExp ? String(val) : val
+  })
+  return createHash('sha1').update(sortedStr).digest('hex')
 }
--- tests/crypto.test.ts
+++ tests/crypto.test.ts
@@ -9,6 +9,7 @@ describe('generateHash()', () => {
     bar: {
       baz: [3, 2, 1],
       qux: 'hello',
+      _id: new ObjectId('5f9b3b3b3b3b3b3b3b3b3b3b'),
       wow: {
         word: 'world',
         hey: {
@@ -16,30 +17,47 @@ describe('generateHash()', () => {
           fred: null,
           missing: undefined
         }
-      },
-      _id: new ObjectId('5f9b3b3b3b3b3b3b3b3b3b3b')
+      }
     }
   }
 
   const data2 = {
+    foo: 42,
     bar: {
-      baz: [1, 2, 3],
+      _id: new ObjectId('5f9b3b3b3b3b3b3b3b3b3b3b'),
+      baz: [3, 2, 1],
+      qux: 'hello',
+      wow: {
+        word: 'world',
+        hey: {
+          waldo: true,
+          fred: null,
+          missing: undefined
+        }
+      }
+    }
+  }
+
+  const data3 = {
+    _id: new ObjectId('5f9b3b3b3b3b3b3b3b3b3b3b'),
+    bar: {
+      qux: 'hello',
+      baz: [3, 2, 1],
       wow: {
         hey: {
           fred: null,
-          missing: undefined,
-          waldo: true
+          waldo: true,
+          missing: undefined
         },
         word: 'world'
-      },
-      qux: 'hello'
+      }
     },
     foo: 42
   }
 
-  const data3 = {
+  const data4 = {
+    _id: new ObjectId('1f9b3b3b3b3b3b3b3b3b3b3b'),
     bar: {
-      _id: new ObjectId('5f9b3b3b3b3b3b3b3b3b3b3b'),
       qux: 'hello',
       baz: [3, 2, 1],
       wow: {
@@ -58,8 +76,10 @@ describe('generateHash()', () => {
     const hash1 = getKey(data1)
     const hash2 = getKey(data2)
     const hash3 = getKey(data3)
+    const hash4 = getKey(data4)
 
-    expect(hash1).not.toEqual(hash2)
-    expect(hash1).toEqual(hash3)
+    expect(hash1).toEqual(hash2)
+    expect(hash1).not.toEqual(hash3)
+    expect(hash3).not.toEqual(hash4)
   })
 })
