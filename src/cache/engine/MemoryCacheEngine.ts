e26d905408d48c401b80c589417384623c44c3ce
ekamahuja <ekamahujau@gmail.com>
2024-02-25 12:31:05 +1000

    Refactor: Replace private keyword with ECMAScript private fields

--- src/cache/engine/MemoryCacheEngine.ts
+++ src/cache/engine/MemoryCacheEngine.ts
@@ -2,14 +2,14 @@ import type IData from '../../interfaces/IData'
 import type ICacheEngine from '../../interfaces/ICacheEngine'
 
 class MemoryCacheEngine implements ICacheEngine {
-  private cache: Map<string, { value: IData, expiresAt: number } | undefined>
+  #cache: Map<string, { value: IData, expiresAt: number } | undefined>
 
   constructor () {
-    this.cache = new Map()
+    this.#cache = new Map()
   }
 
   get (key: string): IData {
-    const item = this.cache.get(key)
+    const item = this.#cache.get(key)
     if (!item || item.expiresAt < Date.now()) {
       this.del(key)
       return undefined
@@ -18,18 +18,18 @@ class MemoryCacheEngine implements ICacheEngine {
   }
 
   set (key: string, value: IData, ttl = Infinity): void {
-    this.cache.set(key, { 
+    this.#cache.set(key, { 
       value, 
       expiresAt: Date.now() + ttl 
     })
   }
 
   del (key: string): void {
-    this.cache.delete(key)
+    this.#cache.delete(key)
   }
 
   clear (): void {
-    this.cache.clear()
+    this.#cache.clear()
   }
 
   close (): void {
