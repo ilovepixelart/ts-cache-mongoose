2ff3b04020b4f113f2ea70995df5ad8056b56733
Alex Eagle <theherocraft@gmail.com>
2024-05-09 10:22:30 +0200

    Lint

--- src/version.ts
+++ src/version.ts
@@ -6,6 +6,7 @@ export const isMongooseLessThan7 = satisfies(mongoose.version, '<7')
 export const convertToObject = (value: unknown): unknown => {
   if (isMongooseLessThan7) {
     if (value != null && typeof value === 'object' && !Array.isArray(value) && value.toObject) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       return value.toObject()
     }
     if (Array.isArray(value)) {
