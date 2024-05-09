a9486cbd58ca14e5a34ef577f5607643ae46067c
Alex Eagle <theherocraft@gmail.com>
2024-05-09 10:50:11 +0200

    Check for all versions

--- src/version.ts
+++ src/version.ts
@@ -4,14 +4,12 @@ import mongoose from 'mongoose'
 export const isMongooseLessThan7 = satisfies(mongoose.version, '<7')
 
 export const convertToObject = (value: unknown): unknown => {
-  if (isMongooseLessThan7) {
-    if (value != null && typeof value === 'object' && !Array.isArray(value) && value.toObject) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      return value.toObject()
-    }
-    if (Array.isArray(value)) {
-      return value.map((doc) => convertToObject(doc))
-    }
+  if (value != null && typeof value === 'object' && !Array.isArray(value) && value.toObject) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    return value.toObject()
+  }
+  if (Array.isArray(value)) {
+    return value.map((doc) => convertToObject(doc))
   }
 
   return value
