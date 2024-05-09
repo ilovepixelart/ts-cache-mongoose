40412003a2dfd552f4239827c5455c2df54563ee
Alex Eagle <theherocraft@gmail.com>
2024-05-09 11:15:03 +0200

    Sequential + bson versions

--- src/version.ts
+++ src/version.ts
@@ -4,12 +4,14 @@ import mongoose from 'mongoose'
 export const isMongooseLessThan7 = satisfies(mongoose.version, '<7')
 
 export const convertToObject = (value: unknown): unknown => {
-  if (value != null && typeof value === 'object' && !Array.isArray(value) && value.toObject) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    return value.toObject()
-  }
-  if (Array.isArray(value)) {
-    return value.map((doc) => convertToObject(doc))
+  if (isMongooseLessThan7) {
+    if (value != null && typeof value === 'object' && !Array.isArray(value) && value.toObject) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      return value.toObject()
+    }
+    if (Array.isArray(value)) {
+      return value.map((doc) => convertToObject(doc))
+    }
   }
 
   return value
