1a90719115ce7d5e439ee5c9f65d3fed38fe80dc
Alex <theherocraft@gmail.com>
2025-01-19 13:39:55 +0100

    Convert cache options to type

--- src/types.ts
+++ src/types.ts
@@ -5,17 +5,17 @@ export type CacheTTL = number | StringValue
 
 export type CacheData = Record<string, unknown> | Record<string, unknown>[] | unknown[] | number | undefined
 
+export type CacheOptions = {
+  engine: 'memory' | 'redis'
+  engineOptions?: RedisOptions
+  defaultTTL?: CacheTTL
+  debug?: boolean
+}
+
 export interface CacheEngine {
   get: (key: string) => Promise<CacheData> | CacheData
   set: (key: string, value: CacheData, ttl?: CacheTTL) => Promise<void> | void
   del: (key: string) => Promise<void> | void
   clear: () => Promise<void> | void
   close: () => Promise<void> | void
-}
-
-export interface CacheOptions {
-  engine: 'memory' | 'redis'
-  engineOptions?: RedisOptions
-  defaultTTL?: CacheTTL
-  debug?: boolean
 }
